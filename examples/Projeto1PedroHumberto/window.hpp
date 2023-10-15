9ae5c3ac479cc00936332685cbcea6a56119aedd
humberto-marinho <humberto.marinho@outlook.com>
2023-10-14 21:11:27 -0300

    Projeto para desenhar primeira versao Hipociclóide

---
+++ examples/Projeto1PedroHumberto/window.hpp
@@ -0,0 +1,27 @@
+#ifndef WINDOW_HPP_
+#define WINDOW_HPP_
+
+#include <random>
+
+#include "abcgOpenGL.hpp"
+
+class Window : public abcg::OpenGLWindow {
+protected:
+  void onCreate() override;
+  void onPaint() override;
+  void onPaintUI() override;
+  void onResize(glm::ivec2 const &size) override;
+  void onDestroy() override;
+
+private:
+  glm::ivec2 m_viewportSize{};
+
+  GLuint m_VAO{};
+  GLuint m_VBOVertices{};
+  GLuint m_program{};
+  glm::vec2 m_P{};
+  void setupModel();
+  float t{};
+};
+
+#endif
